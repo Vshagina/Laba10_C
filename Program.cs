7e6a05b8cbca8beb865cc78d6bf11c1e4af26c1f
Victoria <123573080+Vshagina@users.noreply.github.com>
2024-05-07 09:09:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class FirstClass
+{
+    public event EventHandler<string> MyEvent;
+  
+    public string Name { get; }
+
+    public FirstClass(string name)
+    {
+        Name = name;
+    }
+ 
+    public void RaiseEvent()
+    {
+        MyEvent?.Invoke(this, Name);
+    }
+}
+
+class SecondClass
+{
+ 
+    public void HandleEvent(object sender, string name)
+    {
+        Console.WriteLine($"Событие сгенерировано объектом : {name}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+  
+        FirstClass first1 = new FirstClass("Первый объект");
+        FirstClass first2 = new FirstClass("Второй объект");
+        SecondClass second = new SecondClass();
+
+        first1.MyEvent += second.HandleEvent;
+        first2.MyEvent += second.HandleEvent;
+
+        first1.RaiseEvent();
+        first2.RaiseEvent();
+
+    }
+}
